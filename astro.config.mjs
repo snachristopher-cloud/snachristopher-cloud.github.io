81f1d7aa214bc920b5a14ad8ba301480a6fa01ee
snachristopher-cloud <snachristopher@gmail.com>
2026-02-12 00:49:42 -0600

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://example.com/",
+  site: "https://snachristopher-cloud.github.io",
 
   vite: {
     plugins: [tailwindcss()]
